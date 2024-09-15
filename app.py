93146ede3f01dbbd3eb15f3476c783c973ee4ba0
Isaac <39535421+I2ICreations@users.noreply.github.com>
2024-09-16 00:16:40 +0100

    basic template for a flask api

--- app.py
+++ app.py
@@ -4,8 +4,27 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return 'API SHORTENER!'
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+
+
+@app.route('/retrieve', methods=['GET'])
+def retrieve():
+
+
+@app.route('/update', methods=['PUT'])
+def update():
+
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+
+
+@app.route('/get_statistics', methods=['GET'])
+def get_statistics():
 
 
 if __name__ == '__main__':
